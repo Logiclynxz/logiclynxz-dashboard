4c13f819be37d258ba8f8a67a552f7de2cd07933
Mayki <aw8hurtz@gmail.com>
2024-07-22 22:01:50 +0800

    feat: added middleware

--- middleware.ts
+++ middleware.ts
@@ -2,15 +2,26 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("accessToken");
+  const { pathname } = request.nextUrl;
 
+  const publicRoutes = ["/login"];
+
+  if (publicRoutes.includes(pathname)) {
+    if (token) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+    return NextResponse.next();
+  }
+
+  // For protected routes
   if (!token) {
-    // return NextResponse.redirect(new URL("/login", request.url));
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
   return NextResponse.next();
 }
 
-// protected routes
+// Protected routes
 export const config = {
-  matcher: ["/", "/projects"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
