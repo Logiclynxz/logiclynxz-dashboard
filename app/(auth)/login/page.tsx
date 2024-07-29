44958732ec6bb5dac252a9c4c589c8b9d2af8510
John Micky Butnande <90739127+jmikeyey@users.noreply.github.com>
2024-07-29 20:03:24 +0800

    fix: log error

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -13,12 +13,14 @@ export default function Home() {
         width={300}
         height={300}
         className="absolute animate-spins md:left-[25%] left-5 bottom-[15%] opacity-60 md:bottom-[20%] -z-10"
+        priority={true}
       />
       <Image
         src="/redstar.png"
         alt="redstar"
         width={20}
         height={100}
+        style={{ width: "auto", height: "auto" }} // Maintain aspect ratio
         className="absolute lg:right-80 right-[15%] top-[10%] opacity-60 md:top-10 lg:bottom-0 -z-10"
       />
       {/* <div className="absolute bg-blue-400 h-[400px] lg:h-[450px] w-full -z-10 -bottom-5 blur-[2px] " ></div> */}
