c6da0e73189d673fc6b88057ac6945d410cd48af
John Micky Butnande <90739127+jmikeyey@users.noreply.github.com>
2024-07-30 17:45:47 +0800

    new: added a new utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function capitalizeFirstLetter(string: string) {
+  if (!string) return string; // Handle empty string or null
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
