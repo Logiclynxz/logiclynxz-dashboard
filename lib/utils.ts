c378618cd3683b518e0c0d7dbb66a82a6127c023
John Micky Butnande <90739127+jmikeyey@users.noreply.github.com>
2024-07-31 12:31:36 +0800

    new: added a new utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { format } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -8,4 +9,17 @@ export function cn(...inputs: ClassValue[]) {
 export function capitalizeFirstLetter(string: string) {
   if (!string) return string; // Handle empty string or null
   return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// Format the selected date range
+export function formatDateRange(range: {
+  from: Date | undefined;
+  to: Date | undefined;
+}): string {
+  if (range.from && range.to) {
+    const formattedFrom = format(range.from, "MMM dd, yyyy");
+    const formattedTo = format(range.to, "MMM dd, yyyy");
+    return `${formattedFrom} - ${formattedTo}`;
+  }
+  return "Pick a date range";
 }
