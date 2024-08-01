0c9e1f644ac31cfa928502ba483562fc1866d51e
John Micky Butnande <90739127+jmikeyey@users.noreply.github.com>
2024-08-01 10:44:29 +0800

    fix: image on login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -8,20 +8,22 @@ export default function Home() {
   return (
     <main className="relative  w-full">
       <Image
-        src="/greenpattern.png"
+        src="/assets/greenpattern.png"
         alt="greenpattern"
         width={300}
         height={300}
         className="absolute animate-spins md:left-[25%] left-5 bottom-[15%] opacity-60 md:bottom-[20%] -z-10"
         priority={true}
+        // unoptimized={true}
       />
       <Image
-        src="/redstar.png"
+        src="/assets/redstar.png"
         alt="redstar"
         width={20}
         height={100}
         style={{ width: "auto", height: "auto" }} // Maintain aspect ratio
         className="absolute lg:right-80 right-[15%] top-[10%] opacity-60 md:top-10 lg:bottom-0 -z-10"
+        // unoptimized={true}
       />
       {/* <div className="absolute bg-blue-400 h-[400px] lg:h-[450px] w-full -z-10 -bottom-5 blur-[2px] " ></div> */}
       <div className="container w-full h-full">
